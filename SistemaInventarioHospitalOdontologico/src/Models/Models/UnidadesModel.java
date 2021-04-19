40d6e944be50c3a7cb8208856f4660b8160ef88f
Maryury Zuniga <mg_zunigaz@unicah.edu>
2021-04-19 14:44:29 -0600

    Unidades model

---
+++ SistemaInventarioHospitalOdontologico/src/Models/Models/UnidadesModel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models.Models;
+
+/**
+ *
+ * @author may_g
+ */
+public class UnidadesModel {
+    
+    private Integer UndId;
+    private String UndDescripcion;
+    private String UndEstado;
+
+    public Integer getUndId() {
+        return UndId;
+    }
+
+    public void setUndId(Integer UndId) {
+        this.UndId = UndId;
+    }
+    
+    public String getUndDescripcion() {
+        return UndDescripcion;
+    }
+
+    public void setUndDescripcion(String UndDescripcion) {
+        this.UndDescripcion = UndDescripcion;
+    }
+
+    public String getUndEstado() {
+        return UndEstado;
+    }
+
+    public void setUndEstado(String UndEstado) {
+        this.UndEstado = UndEstado;
+    }
+    
+    
+}

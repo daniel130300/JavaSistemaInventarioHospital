53d75b3de920bb5bad73111cdc01e141c94cd466
Hector Daniel <daniel130300@outlook.com>
2021-04-19 14:27:36 -0600

    Cambios script bdd

--- Script Bdd/Scripts Individuales/10 - inventariohospital_loteproductos.sql
+++ Script Bdd/Scripts Individuales/10 - inventariohospital_loteproductos.sql
@@ -1,10 +1,10 @@
-CREATE TABLE `bitacoracatalogoproductos` (
+CREATE TABLE `bitacoraloteproductos` (
   `BcpId` int NOT NULL AUTO_INCREMENT,
   `UsrUsuario` char(40) NOT NULL,
   `BcpAccion` varchar(5) NOT NULL,
   `BcpDescripcionCambios` varchar(300) DEFAULT NULL,
   `BcpFecha` date NOT NULL,
-  `PrdId` int DEFAULT NULL,
+  `LprId` int DEFAULT NULL,
   PRIMARY KEY (`BcpId`),
-  KEY `IX_Relationship1` (`PrdId`)
+  KEY `IX_Relationship2` (`LprId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- Script Bdd/Scripts Individuales/13 - inventariohospital_detallebitacoraloteproductos.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `detallebitacoraloteproductos` (
-  `BcpId` int NOT NULL,
-  `LprId` int NOT NULL,
-  PRIMARY KEY (`BcpId`,`LprId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;

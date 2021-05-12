e90d03b5e504c27443b72da70811d50eada4e848
katering08 <77954345+katering08@users.noreply.github.com>
2021-05-12 17:27:41 -0600

    Creación InventarioHijoModel

---
+++ SistemaInventarioHospitalOdontologico/src/Models/Models/InventarioHijoModel.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models.Models;
+
+/**
+ *
+ * @author may_g
+ */
+public class InventarioHijoModel {
+    private Integer InvHId;
+    private String InvHDescripcion;
+    private Integer InvHCantidad;
+    private Integer InvPId;
+    private String UndId;
+
+    public Integer getInvHId() {
+        return InvHId;
+    }
+
+    public void setInvHId(Integer InvHId) {
+        this.InvHId = InvHId;
+    }
+
+    public String getInvHDescripcion() {
+        return InvHDescripcion;
+    }
+
+    public void setInvHDescripcion(String InvHDescripcion) {
+        this.InvHDescripcion = InvHDescripcion;
+    }
+
+    public Integer getInvHCantidad() {
+        return InvHCantidad;
+    }
+
+    public void setInvHCantidad(Integer InvHCantidad) {
+        this.InvHCantidad = InvHCantidad;
+    }
+
+    
+    public Integer getInvPId() {
+        return InvPId;
+    }
+
+    public void setInvPId(Integer InvPId) {
+        this.InvPId = InvPId;
+    }
+
+    public String getUndId() {
+        return UndId;
+    }
+
+    public void setUndId(String UndId) {
+        this.UndId = UndId;
+    }
+
+   
+    
+}

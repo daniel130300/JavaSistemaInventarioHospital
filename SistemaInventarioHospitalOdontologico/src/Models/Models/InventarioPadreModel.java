927cf959d409ffa9109f5895688d3edbc55af94c
katering08 <77954345+katering08@users.noreply.github.com>
2021-05-11 18:56:43 -0600

    Actualización InventarioPadreModel

---
+++ SistemaInventarioHospitalOdontologico/src/Models/Models/InventarioPadreModel.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Models.Models;
+
+/**
+ *
+ * @author usuario
+ */
+public class InventarioPadreModel {
+    
+    private Integer InvPId;
+    private String InvPCodInterno;
+    private String InvPDescripcion;
+    private String InvPFchCaducidad;
+    private Integer InvPCantidad;
+    private String UndDescripcion;
+    private Integer KitId;
+    private String KitNombre;
+    private Integer PrdId;
+    private String PrdNombre;
+
+    public Integer getInvPId() {
+        return InvPId;
+    }
+
+    public void setInvPId(Integer InvPId) {
+        this.InvPId = InvPId;
+    }
+
+    public String getInvPCodInterno() {
+        return InvPCodInterno;
+    }
+
+    public void setInvPCodInterno(String InvPCodInterno) {
+        this.InvPCodInterno = InvPCodInterno;
+    }
+
+    public String getInvPDescripcion() {
+        return InvPDescripcion;
+    }
+
+    public void setInvPDescripcion(String InvPDescripcion) {
+        this.InvPDescripcion = InvPDescripcion;
+    }
+
+    public String getInvPFchCaducidad() {
+        return InvPFchCaducidad;
+    }
+
+    public void setInvPFchCaducidad(String InvPFchCaducidad) {
+        this.InvPFchCaducidad = InvPFchCaducidad;
+    }
+
+    public Integer getInvPCantidad() {
+        return InvPCantidad;
+    }
+
+    public void setInvPCantidad(Integer InvPCantidad) {
+        this.InvPCantidad = InvPCantidad;
+    }
+
+    public String getUndDescripcion() {
+        return UndDescripcion;
+    }
+
+    public void setUndDescripcion(String UndDescripcion) {
+        this.UndDescripcion = UndDescripcion;
+    }
+
+    public Integer getKitId() {
+        return KitId;
+    }
+
+    public void setKitId(Integer KitId) {
+        this.KitId = KitId;
+    }
+
+    public String getKitNombre() {
+        return KitNombre;
+    }
+
+    public void setKitNombre(String KitNombre) {
+        this.KitNombre = KitNombre;
+    }
+
+    public Integer getPrdId() {
+        return PrdId;
+    }
+
+    public void setPrdId(Integer PrdId) {
+        this.PrdId = PrdId;
+    }
+
+    public String getPrdNombre() {
+        return PrdNombre;
+    }
+
+    public void setPrdNombre(String PrdNombre) {
+        this.PrdNombre = PrdNombre;
+    }
+
+    
+    
+}
